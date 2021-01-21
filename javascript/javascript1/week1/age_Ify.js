a6f6a7d0e20960fd17cde4d68857380d18dfdb61
kinan966 <ayaanle.a.hassan@gmail.com>
2021-01-21 10:12:41 +0100

    A future age calculator exercice finished

---
+++ javascript/javascript1/week1/age_Ify.js
@@ -0,0 +1,11 @@
+
+var yearOfBirth = 1987;
+console.log(yearOfBirth);
+var yearFuture = 2027;
+console.log(yearFuture);
+
+var age = (yearFuture - yearOfBirth);
+
+console.log("You will be " + age + " old in 2027");
+
+
