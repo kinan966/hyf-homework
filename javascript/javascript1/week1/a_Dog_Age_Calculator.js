412c7164277799c77de66771247f775f87d6bfe1
kinan966 <ayaanle.a.hassan@gmail.com>
2021-01-21 10:13:35 +0100

    A dog age calculator exercice finished

---
+++ javascript/javascript1/week1/a_Dog_Age_Calculator.js
@@ -0,0 +1,14 @@
+
+var dogYearOfBirth = 2017; 
+var dogYearFuture = 2027;
+var dogYear;
+var shouldShowResultInDogYears;
+Boolean (shouldShowResultInDogYears);
+
+var dogYear = (dogYearFuture - dogYearOfBirth);
+var shouldShowResultInDogYears = (dogYearFuture - dogYearOfBirth) * 7;
+
+
+console.log("Your dog will be " + dogYear + " human years old in 2027");
+console.log("Or your dog will be " + shouldShowResultInDogYears + 
+" human years old in 2027");
