4dac8f1b099743d6c0f782707c4e9da859227ddb
kinan966 <ayaanle.a.hassan@gmail.com>
2021-01-29 11:33:39 +0100

    eventApplication fille

---
+++ javascript/javascript1/week2/eventApplication.js
@@ -0,0 +1,22 @@
+
+const day = new Date();
+const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+function getEventWeekday(eventDay)
+{
+
+  const n = ((day.getDay() + eventDay) % 7);
+
+  return weekday[n];
+}
+
+
+console.log(getEventWeekday(2));
+console.log(getEventWeekday(25));
